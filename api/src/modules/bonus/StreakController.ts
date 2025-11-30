0f5c8267de58c0ef4356f0793d175eec9d99f1cb
Vitor Lins <jvlinsbaracho13@gmail.com>
2025-11-30 11:47:06 -0300

    feat: create StreakController class

---
+++ api/src/modules/bonus/StreakController.ts
@@ -0,0 +1,23 @@
+import { type Request, type Response } from "express";
+import { StreakService } from "./StreakService.js";
+
+export class StreakController {
+  constructor(private streakService: StreakService = new StreakService()) {}
+
+  async getMultiplier(req: Request, res: Response) {
+    try {
+      const { userId } = req.body; 
+
+      if (!userId) {
+        return res.status(400).json({ error: "User ID is required" });
+      }
+
+      const result = await this.streakService.calculateMultiplier(userId);
+
+      return res.status(200).json(result);
+    } catch (error) {
+      console.error(error); 
+      return res.status(500).json({ error: "Error calculating streak multiplier" });
+    }
+  }
+}
